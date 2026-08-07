ddbc5dcf79f25b0de3eb90d1e4f0375013703447
Stefano Guerrini <stefano@zenrows.com>
2026-08-07 20:47:56 +0200

    Fix unit test for encoded redirect URLs

--- Tests/LinkStripTests/URLCleanerTests.swift
+++ Tests/LinkStripTests/URLCleanerTests.swift
@@ -76,7 +76,7 @@ final class URLCleanerTests: XCTestCase {
     }
 
     func testCleanRedirectExtractsEncodedDestinationFromPath() {
-        let input = "https://link.fndrsp.net/CL0/https:%2F%2Fexample.com%2Fpage%3Futm_source%3Dnewsletter%26emailLog%3Dabc/1/0100019fdc55b137/O_ycuXgrQ5eekmWOYczw"
+        let input = "https://link.fndrsp.net/CL0/https:%2F%2Fexample.com%2Fpage%3Futm_source%3Dnewsletter%26fbclid%3Dabc/1/0100019fdc55b137/O_ycuXgrQ5eekmWOYczw"
         XCTAssertEqual(
             cleaner.cleanRedirect(input),
             "https://example.com/page"
